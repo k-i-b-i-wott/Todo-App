df674e352e4da7d40e09201cdd20588ab7176404
Kibiwott <bk.kibiwott@gmail.com>
2025-03-19 21:26:02 +0300

    Added the number of incomplete tasks counter

--- src/todo/Hero.jsx
+++ src/todo/Hero.jsx
@@ -2,12 +2,20 @@ import React from 'react'
 import { now } from '../utils/now'
 import TodoInput from './TodoInput'
 import TodoItems from './TodoItems'
+import useTaskstore from '../store/taskStore'
 const Hero = () => {
+  const task=useTaskstore((state)=>state.tasks);
+  let numberofIncompleteTasks=0;
+  task.forEach(function(task){
+    if(task.completed===false){
+      numberofIncompleteTasks=numberofIncompleteTasks+1;
+    }
+  })
   return (
    <div className='todo-page'>
      <section className='hero'>
          <h1>{now()} Tanui</h1>
-      <h4>You have 5 todos remaining today</h4>
+      <h4>You have {numberofIncompleteTasks} todos remaining today</h4>
       <TodoInput />
       </section>
       <div className="todo-items-display">
